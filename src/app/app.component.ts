32dc1550e0ab778c45bccaab7b2fff6395986bc1
#PSP <pspofficial29@gmail.com>
2021-07-11 12:45:34 +0530

    events in angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angularPlayground';
+  title = '#PSP';
+  topic = 'angular';
+  click_name = "PSP";
+  arr = ['psp', 'abc', 'pqr']
+  getArr() {
+    return this.arr;
+  }
+  object = {
+    name: this.title,
+    topic: this.topic
+  }
+  getEvent(arg: any) {
+    alert(arg);
+  }
+  myEvent(evt: any) {
+    console.log(evt);
+  }
+  getVal(val: any) {
+    console.log(val);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-
+import { UsersModule } from './users/users.module';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    UsersModule
   ],
   providers: [],
   bootstrap: [AppComponent]
